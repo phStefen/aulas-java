f5a64fca14995e1361d573a64b7c6e97e9b311fa
StBitola <raph.stefen@gmail.com>
2020-07-30 19:07:27 -0300

    Update LED.java

--- projetos/aula-09/LED/src/led/LED.java
+++ projetos/aula-09/LED/src/led/LED.java
@@ -12,18 +12,19 @@ public class LED {
         int[] x = {6,2,5,5,4,5,6,3,7,6};
         int l=0;
         
+
         while (l<1 || l>1000){
             System.out.print("Digite quantas Linhas de LEDs: ");
             l = new Scanner(System.in).nextInt();
-            
+
             if(l<1 || l>1000) System.out.println("Valor Inválido!");
         }
-        
+
         for(int i=0;i<l;i++){
             System.out.print("\nDigite o Número de Exibição: ");
             char[] numeros = new Scanner(System.in).next().toCharArray();
             int led=0;
-            
+
             for(char n : numeros){
                 led+=x[Character.getNumericValue(n)];
             }
